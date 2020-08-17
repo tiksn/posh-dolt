f33a25988d7b6b9355b73fe28a0b64766a2c7d4d
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-08-17 17:39:02 +0400

    check version

---
+++ src/CheckRequirements.ps1
@@ -0,0 +1,22 @@
+$Global:DoltMissing = $false
+
+$requiredVersion = [Version]'0.18.3'
+$script:DoltVersion = $requiredVersion
+
+if (!(Get-Command dolt -TotalCount 1 -ErrorAction SilentlyContinue)) {
+    Write-Warning "dolt command could not be found. Please create an alias or add it to your PATH."
+    $Global:DoltMissing = $true
+    return
+}
+
+if ([string](dolt version 2> $null) -match '(?<ver>\d+(?:\.\d+)+)') {
+    $script:DoltVersion = [System.Version]$Matches['ver']
+}
+
+if ($GitVersion -lt $requiredVersion) {
+    Write-Warning "posh-dolt requires Dolt $requiredVersion or better. You have $GitVersion."
+    $false
+}
+else {
+    $true
+}
