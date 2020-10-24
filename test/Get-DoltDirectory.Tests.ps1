18c50b8b08c8b4ecfb95ec9970cf7e9507706822
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-10-24 21:21:55 +0300

    added Get-DoltDirectory Tests

---
+++ test/Get-DoltDirectory.Tests.ps1
@@ -0,0 +1,70 @@
+BeforeAll {
+    . $PSScriptRoot\Shared.ps1
+}
+
+Describe 'Get-DoltDirectory Tests' {
+    Context "Test normal repository" {
+        BeforeAll {
+            [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssigments', '')]
+            $origPath = Get-Location
+        }
+        AfterAll {
+            Set-Location $origPath
+        }
+
+        It 'Returns $null for not a Dolt repo' {
+            Set-Location $env:windir
+            Get-DoltDirectory | Should -BeNullOrEmpty
+        }
+        It 'Returns $null for not a filesystem path' {
+            Set-Location Alias:\
+            Get-DoltDirectory | Should -BeNullOrEmpty
+        }
+        It 'Returns correct path when in the root of repo' {
+            $repoRoot = (Resolve-Path $env:PoshDoltTestRepository).Path
+            Set-Location $repoRoot
+            Get-DoltDirectory | Should -BeExactly (MakeNativePath $repoRoot\.dolt)
+        }
+    }
+
+    Context 'Test bare repository' {
+        BeforeAll {
+            [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssigments', '')]
+            $origPath = Get-Location
+            $temp = [System.IO.Path]::GetTempPath()
+            $bareRepoName = "test.dolt"
+            $bareRepoPath = Join-Path $temp $bareRepoName
+            if (Test-Path $bareRepoPath) {
+                Remove-Item $bareRepoPath -Recurse -Force
+            }
+            New-Item $bareRepoPath -ItemType Directory | Out-Null
+            Push-Location
+            Set-Location $bareRepoPath
+            &$doltbin init --name "Pester User" --email "you@example.com"
+            Pop-Location
+
+            Remove-Item Env:\DOLT_DIR -ErrorAction SilentlyContinue
+        }
+        AfterAll {
+            Set-Location $origPath
+            if (Test-Path $bareRepoPath) {
+                Remove-Item $bareRepoPath -Recurse -Force
+            }
+        }
+
+        It 'Returns correct path when in the root of bare repo' {
+            Set-Location $bareRepoPath
+            Get-DoltDirectory | Should -BeExactly $bareRepoPath
+        }
+    }
+
+    Context "Test DOLT_DIR environment variable" {
+        AfterAll {
+            Remove-Item Env:\DOLT_DIR -ErrorAction SilentlyContinue
+        }
+        It 'Returns the value in DOLT_DIR env var' {
+            $Env:DOLT_DIR = MakeNativePath '/xyzzy/posh-dolt/'
+            Get-DoltDirectory | Should -BeExactly $Env:DOLT_DIR
+        }
+    }
+}
